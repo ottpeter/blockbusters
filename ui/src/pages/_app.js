6eaa5838ce77c017a604d049dfcba772e2cb1ae5
Fruzsina Lenti <fruzsina.lenti@vacuumlabs.com>
2023-12-04 14:10:39 +0100

    adds material-ui and polkadot js

---
+++ ui/src/pages/_app.js
@@ -0,0 +1,73 @@
+import Head from "next/head";
+import { CacheProvider } from "@emotion/react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { AuthConsumer, AuthProvider } from "src/contexts/auth-context";
+import { useNProgress } from "src/hooks/use-nprogress";
+import { createTheme } from "src/theme";
+import { createEmotionCache } from "src/utils/create-emotion-cache";
+import "simplebar-react/dist/simplebar.min.css";
+import { useEffect } from "react";
+const { ApiPromise, WsProvider } = require("@polkadot/api");
+
+const clientSideEmotionCache = createEmotionCache();
+
+const SplashScreen = () => null;
+
+async function main() {
+  // Initialise the provider to connect to the local node
+  const provider = new WsProvider("ws://127.0.0.1:9944");
+
+  // Create the API and wait until ready
+  const api = await ApiPromise.create({ provider });
+
+  // Retrieve the chain & node information information via rpc calls
+  const [chain, nodeName, nodeVersion] = await Promise.all([
+    api.rpc.system.chain(),
+    api.rpc.system.name(),
+    api.rpc.system.version(),
+  ]);
+
+  console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
+}
+
+const App = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  useEffect(() => {
+    main()
+      .catch(console.error)
+      .finally(() => console.log("exit"));
+  }, []);
+
+  useNProgress();
+
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  const theme = createTheme();
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Devias Kit</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <AuthProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <AuthConsumer>
+              {(auth) =>
+                auth.isLoading ? <SplashScreen /> : getLayout(<Component {...pageProps} />)
+              }
+            </AuthConsumer>
+          </ThemeProvider>
+        </AuthProvider>
+      </LocalizationProvider>
+    </CacheProvider>
+  );
+};
+
+export default App;
