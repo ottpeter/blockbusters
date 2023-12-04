ba77d9321699acd62cc2e4266e18c5bf3d649171
Peter Molnar <mpeteriii@gmail.com>
2023-12-05 00:15:53 +0100

    feat: add IdentityHandler address

--- contract/deploy/100_GetContracts.js
+++ contract/deploy/100_GetContracts.js
@@ -7,11 +7,13 @@ module.exports = async function ({ deployments, getChainId }) {
 
   const DaoFactory = (await deployments.get("DaoFactory")).address;
   const DaoRoot = (await deployments.get("DaoRoot")).address;
+  const IdentityHandler = (await deployments.get("IdentityHandler")).address;
 
 
   const contracts = {
     DaoFactory,
-    DaoRoot
+    DaoRoot,
+    IdentityHandler
   };
   console.table(contracts);
 };
